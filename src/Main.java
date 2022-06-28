da626009a2bd515f9ce2218ec64aa085d7cd3b9a
Ksushka1993 <buslovdn@gmail.com>
2022-06-28 23:27:35 +0700

    Added calculate bonus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int balance = 100;
+        int addedAmount = 5000;
+        int bonus = addedAmount / 100;
+        int total = (balance + addedAmount);
+        if (addedAmount > 0) {
+            if (addedAmount > 1000) {
+                System.out.println("Начислено бонусов: " + bonus);
+                System.out.println("Общая сумма: " + (total + bonus) + "рублей");
+            } else {
+                System.out.println("Нет начисленных бонусов");
+                System.out.println("Общая сумма: " + total + "рублей");
+
+            }
+        } else {
+            System.out.println("Сумма зачислений должна быть положительная");
+        }
     }
 }
